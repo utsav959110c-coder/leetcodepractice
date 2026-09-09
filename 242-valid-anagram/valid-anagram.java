00f87517515d7a366b247f40a17c62c6a73bf064
utsav959110c-coder <utsav9591.10c@gmail.com>
2026-09-09 21:08:05 +0530

    Time: 4 ms (83.82%) | Memory: 46.2 MB (46.86%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] s1=s.toCharArray();
+        char[] s2=t.toCharArray();
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+         if(Arrays.equals(s1,s2)){
+           return true;
+         }
+        return false;
+    
+    }
+}
