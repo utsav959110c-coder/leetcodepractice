e1505ae28662c725cbf83d557194fbe2f2728335
utsav959110c-coder <utsav9591.10c@gmail.com>
2026-09-12 20:40:22 +0530

    Time: 1 ms (95.57%) | Memory: 42.8 MB (63.17%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i=0;
+        int j=0;
+        while(i<s.length()){
+            while(j<t.length() && s.charAt(i)!=t.charAt(j)){
+                j++;
+            }
+            if(j==t.length()){
+                return false;
+            }
+            i++;
+            j++;
+        }
+        return true;
+    }
+}
